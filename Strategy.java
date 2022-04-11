e29a123139f08e6d5781e3294b8814de291aa1e9
Akzhol Issaliyev <akzholbusiness@gmail.com>
2022-04-11 06:59:45 +0600

    strategy

---
+++ Strategy.java
@@ -0,0 +1,57 @@
+interface Activity {
+    void doJob();
+}
+
+class Akzhol {
+    Activity activity;
+    
+    void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    void executeJob(){
+        activity.doJob();
+    }
+}
+
+class Eat implements Activity {
+    public void doJob() {
+        System.out.println("Eat...");
+    }
+}
+
+class Sleep implements Activity {
+    public void doJob() {
+        System.out.println("Sleep...");
+    }
+}
+
+class Code implements Activity {
+    public void doJob() {
+        System.out.println("Code...");
+    }
+}
+
+class Repeat implements Activity {
+    public void doJob() {
+        System.out.println("Repeat...");
+    }
+}
+
+class Strategy {
+    public static void main(String[] args) {
+        Akzhol akzhol = new Akzhol();
+        
+        akzhol.setActivity(new Eat());
+        akzhol.executeJob();
+
+        akzhol.setActivity(new Sleep());
+        akzhol.executeJob();
+        
+        akzhol.setActivity(new Code());
+        akzhol.executeJob();
+
+        akzhol.setActivity(new Repeat());
+        akzhol.executeJob();
+    }
+}
