8589a97d5d1bccd509c9753e93336d2ea6562348
Akzhol Issaliyev <akzholbusiness@gmail.com>
2022-04-11 06:38:52 +0600

    adapter

---
+++ Adapter.java
@@ -0,0 +1,35 @@
+class DataOne {
+
+}
+
+class DataTwo {
+
+}
+
+class AccountImpl{
+    String parse() {
+        return "parsed!";
+    }
+}
+
+interface Account {
+    String parseData();
+}
+
+class AccountImplAdapter implements Account {
+    AccountImpl account;
+    AccountImplAdapter(AccountImpl account){
+        this.account = account;
+    }
+    public String parseData() {
+        // converting unsuitable method to suitable to interface
+        return account.parse();
+    }
+}
+
+class Adapter {
+    public static void main(String[] args) {
+        Account account = new AccountImplAdapter(new AccountImpl());
+        System.out.println(account.parseData());
+    }
+}
