2e7e3b64f69bf90d9a508900369b77896d204fc8
Akzhol Issaliyev <akzholbusiness@gmail.com>
2022-04-11 06:29:57 +0600

    Adapter

--- structuralPatterns/Adapter.java
+++ structuralPatterns/Adapter.java
@@ -30,8 +30,6 @@ class AccountImplAdapter implements Account {
 
 class Adapter {
     public static void main(String[] args) {
-        Account account = new AccountImplAdapter(new AccountImpl());
-
-        System.out.println(account.parseData());
+        
     }
 }
